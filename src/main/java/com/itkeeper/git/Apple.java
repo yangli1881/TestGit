4a1d4302e42f1ab2a6eeda9fc7897450bcc62a54
eclipse.user <eclipse_user@qq.com>
2019-08-04 23:42:08 +0800

    my first git commit in eclipse

---
+++ src/main/java/com/itkeeper/git/Apple.java
@@ -0,0 +1,26 @@
+package com.itkeeper.git;
+
+public class Apple {
+	private int id;
+	private float price;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/test/java/com/itkeeper/git/GoodTest.java
@@ -0,0 +1,12 @@
+package com.itkeeper.git;
+
+import org.junit.Test;
+
+public class GoodTest {
+	
+	@Test
+	public void testFoo() {
+		System.out.println("测试通过！");
+	}
+
+}
