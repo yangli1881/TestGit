271ef6ceb388f5daa57e782e87cdfe6838ebf847
yangli01 <yangli01_email@qq.com>
2019-08-07 22:20:24 +0800

    test conflict in right

--- src/main/java/com/itkeeper/git/Happy.java
+++ src/main/java/com/itkeeper/git/Happy.java
@@ -3,7 +3,7 @@ package com.itkeeper.git;
 public class Happy {
 
 	public static void main(String[] args) {
-		System.out.println("right.....");
+		System.out.println("right.....");                                                         
 	}
 
 }
